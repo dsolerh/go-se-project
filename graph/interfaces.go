e0ff5da890dcd7eb6800ecae29dbd7c66a3b65c2
dsolerh <dsolerh.cinter95@gmail.com>
2024-04-11 10:12:27 +0300

    feat: define the interfaces and errors for the graph feature

---
+++ graph/interfaces.go
@@ -0,0 +1,42 @@
+package graph
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Graph interface {
+	// links
+	UpsertLink(link *Link) error
+	FindLink(id uuid.UUID) (*Link, error)
+	Links(fromId, toId uuid.UUID, retrieveBefore time.Time) (Iterator[*Link], error)
+	// edges
+	UpsertEdge(edge *Edge) error
+	RemoveStaleEdges(fromId uuid.UUID, updateBefore time.Time) error
+	Edges(fromId, toId uuid.UUID, updateBefore time.Time) (Iterator[*Edge], error)
+}
+
+type Link struct {
+	Id          uuid.UUID
+	Url         string
+	RetrievedAt time.Time
+}
+
+type Edge struct {
+	Id        uuid.UUID
+	Src, Dst  uuid.UUID
+	UpdatedAt time.Time
+}
+
+type Iterator[T any] interface {
+	// gets the item of the iterator
+	Item() T
+	// Next advances the iterator. If no more items are available or an
+	// error occurs, calls to Next() return false.
+	Next() bool
+	// Error returns the last error encountered by the iterator.
+	Error() error
+	// Close releases any resources associated with an iterator.
+	Close() error
+}
